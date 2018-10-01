8b082e3ed5a72fb79e6658e0c904a2776e116537
Max Harper <maxharp3r@gmail.com>
2018-10-01 06:15:45 -0500

    add 404 handler

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, jsonify, redirect, render_template, request, url_for
+from flask import Flask, abort, jsonify, redirect, render_template, request, url_for
 import psycopg2
 
 import db
@@ -10,6 +10,10 @@ app = Flask(__name__)
 @app.before_first_request
 def initialize():
     db.setup()
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
 
 @app.route('/')
 def home():
@@ -23,7 +27,19 @@ def movie(movie_id):
     with db.get_db_cursor() as cur:
         cur.execute("SELECT * FROM movie where movie_id=%s", (movie_id,))
         movie = cur.fetchone()
+
+    if not movie:
+        return abort(404)
+
     return render_template("movie.html", movie=movie)
+
+@app.route('/genres/<genre>')
+def genre(genre):
+    with db.get_db_cursor() as cur:
+        cur.execute("SELECT * FROM movie where genre=%s", (genre,))
+        movies = [record for record in cur]
+    return render_template("home.html", movies=movies)
+
 
 if __name__ == '__main__':
     pass
